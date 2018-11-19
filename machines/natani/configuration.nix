eb043d5a42ad3c18394bacdd1be00c73c0d60f6c
talyz <kim.lindberger@gmail.com>
2018-11-19 17:04:44 +0100

    Natani: Enable automatic screen rotation

--- machines/natani/configuration.nix
+++ machines/natani/configuration.nix
@@ -103,6 +103,9 @@
   # allowedUDPPorts = [ ... ];
   };
 
+  # Enable automatic screen rotation.
+  hardware.sensor.iio.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.bash.enableCompletion = true;
