b3e104ca31d8d8b1c587b9dfc7268ec1d89549fa
talyz <kim.lindberger@gmail.com>
2018-11-29 09:49:12 +0100

    Move common stuff to common.nix

--- profiles/common.nix
+++ profiles/common.nix
@@ -3,7 +3,6 @@
 {
   nix.buildCores = 0;
   
-  # List packages installed in system profile.
   environment.systemPackages = with pkgs; [
     wget
     ag
@@ -29,10 +28,29 @@
     msmtp
   ];
 
+  # Internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "dvorak-sv-a1";
+    defaultLocale = "en_US.UTF-8";
+  };
+  
+  time.timeZone = "Europe/Stockholm";
+
+  nixpkgs.config.allowUnfree = true;
+  
   programs.fish.enable = true;
 
   services.emacs = {
     enable = true;
     defaultEditor = true;
   };
+
+  users.extraUsers.talyz = {
+    isNormalUser = true;
+		extraGroups = [ "wheel" ];
+		shell = pkgs.fish;
+		uid = 1000;
+		initialPassword = "aoeuaoeu";
+	};
 }
