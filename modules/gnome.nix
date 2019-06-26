c97f123862af7194b07f5e8556f9e49a1e81002a
talyz <kim.lindberger@gmail.com>
2019-06-26 13:51:01 +0200

    gnome: Set switch-to-application keybindings to empty
    Needed in gnome 3.32, since they're in conflict with my
    switch-to-workspace keybindings.

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -61,6 +61,16 @@ in
               switch-windows=['<Alt>Tab']
               switch-applications=[]
 
+              [org.gnome.shell.keybindings]
+              switch-to-application-1=[]
+              switch-to-application-2=[]
+              switch-to-application-3=[]
+              switch-to-application-4=[]
+              switch-to-application-5=[]
+              switch-to-application-6=[]
+              switch-to-application-7=[]
+              switch-to-application-8=[]
+              switch-to-application-9=[]
 
               [org.gnome.desktop.wm.preferences]
               num-workspaces=8
