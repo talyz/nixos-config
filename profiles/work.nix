f2cfda3d57880a656cedacc58d85509cec6d6bcf
talyz <kim.lindberger@gmail.com>
2020-02-23 14:45:46 +0100

    work: Add myself to the vboxusers group

--- profiles/work.nix
+++ profiles/work.nix
@@ -8,5 +8,12 @@
       nomachine-client
     ];
 
+  # virtualisation.libvirtd.enable = true;
+  # virtualisation.libvirtd.qemuRunAsRoot = false;
+  # virtualisation.libvirtd.onShutdown = "shutdown";
+  # users.users.talyz.extraGroups = [ "libvirtd" ];
+  # networking.firewall.checkReversePath = false;
+
   virtualisation.virtualbox.host.enable = true;
+  users.users.talyz.extraGroups = [ "vboxusers" ];
 }
