7d3685cbfaf4101265be74f26ca3ec8a597ef3ce
talyz <kim.lindberger@gmail.com>
2020-12-12 12:14:59 +0100

    emacs: Don't use an overlay for emacs
    It occationally creates problems and just installing the package
    globally works well enough.

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -5,34 +5,41 @@ with lib;
 let
   cfg = config.talyz.emacs;
 
-  overlay = self: super:
-    {
-      emacs = (super.emacsWithPackagesFromUsePackage {
-        config = ../home-talyz-nixpkgs/dotfiles/emacs/emacs-config.org;
-        package = super.emacs;
-        extraEmacsPackages = cfg.extraPackages;
-        override = epkgs: epkgs // {
-          # weechat = epkgs.melpaPackages.weechat;
-          weechat =  epkgs.melpaPackages.weechat.overrideAttrs (oldAttrs: oldAttrs // {
-            src = super.fetchFromGitHub {
-              owner = "bqv";
-              repo = "weechat.el";
-              rev = "446868de424170be0584980d4dcc0859f7077d54";
-              sha256 = "12vbp35z3hgr9lqplc7ycf8n2rfd0zarr50arc9bqy5z14pf3biv";
-            };
-          });
-          elpy = epkgs.melpaPackages.elpy;
-          dracula-theme = epkgs.melpaPackages.dracula-theme.overrideAttrs (oldAttrs: oldAttrs // {
-            src = super.fetchFromGitHub {
-              owner = "talyz";
-              repo = "dracula-emacs";
-              rev = "a4c41005cb58208932b6d9ec99c8d912e42c634a";
-              sha256 = "0bp3adn9w08zrps0dgwhcmwcifzld8lsq92rsad5hcvnqhvjfkc7";
-            };
-          });
+  emacs = (pkgs.emacsWithPackagesFromUsePackage {
+    config = ../home-talyz-nixpkgs/dotfiles/emacs/emacs-config.org;
+    package = pkgs.emacs;
+    extraEmacsPackages = epkgs: [ epkgs.inf-elixir ] ++ (cfg.extraPackages epkgs);
+    override = epkgs: epkgs // {
+      # weechat = epkgs.melpaPackages.weechat;
+      weechat =  epkgs.melpaPackages.weechat.overrideAttrs (oldAttrs: oldAttrs // {
+        src = pkgs.fetchFromGitHub {
+          owner = "bqv";
+          repo = "weechat.el";
+          rev = "446868de424170be0584980d4dcc0859f7077d54";
+          sha256 = "12vbp35z3hgr9lqplc7ycf8n2rfd0zarr50arc9bqy5z14pf3biv";
         };
       });
+      elpy = epkgs.melpaPackages.elpy;
+      dracula-theme = epkgs.melpaPackages.dracula-theme.overrideAttrs (oldAttrs: oldAttrs // {
+        src = /persistent/home/talyz/Projects/dracula-emacs;
+        # pkgs.fetchFromGitHub {
+        #   owner = "talyz";
+        #   repo = "dracula-emacs";
+        #   rev = "a4c41005cb58208932b6d9ec99c8d912e42c634a";
+        #   sha256 = "0bp3adn9w08zrps0dgwhcmwcifzld8lsq92rsad5hcvnqhvjfkc7";
+        # };
+      });
+      inf-elixir = epkgs.trivialBuild {
+        pname = "inf-elixir";
+        src = pkgs.fetchFromGitHub {
+          owner = "J3RN";
+          repo = "inf-elixir";
+          rev = "a257ebd5a4c6bda82cf086069c36f2e1ae02eb6f";
+          sha256 = "05f0h6jrzkjshv444kz80vaqnm1800ykiibhkkgq08nff7icz05f";
+        };
+      };
     };
+  });
 in
 {
   options =
@@ -60,9 +67,8 @@ in
       nixpkgs.overlays =
         [
           (import ./emacs-overlay)
-          overlay
         ];
 
-      environment.systemPackages = [ pkgs.emacs ];
+      environment.systemPackages = [ emacs ];
     };
 }
