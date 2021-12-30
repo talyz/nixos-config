38fa3820468e6fdcc2cbe5d66f1cb7c23416a375
talyz <kim.lindberger@gmail.com>
2021-12-30 14:50:21 +0100

    common: Use the aarch64 community builder

--- modules/common.nix
+++ modules/common.nix
@@ -19,7 +19,25 @@
 
     nix.extraOptions = ''
       experimental-features = nix-command flakes
+      builders-use-substitutes = true
     '';
+
+    programs.ssh.knownHosts."aarch64.nixos.community".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUTz5i9u5H2FHNAmZJyoJfIGyUm/HfGhfwnc142L3ds";
+
+    nix = {
+      distributedBuilds = true;
+      buildMachines = [
+        {
+          hostName = "aarch64.nixos.community";
+          maxJobs = 64;
+          sshKey = "/etc/nix/id_ed25519";
+          sshUser = "talyz";
+          system = "aarch64-linux";
+          supportedFeatures = [ "big-parallel" "kvm" "nixos-test" ];
+        }
+      ];
+    };
 
     environment.systemPackages = with pkgs; [
       wget
