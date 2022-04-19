2d0c0f2e0b99f6c3b65095e52a8f465f9d606e4d
talyz <kim.lindberger@gmail.com>
2022-04-19 17:55:45 +0200

    common-graphical: Don't use iwd

--- modules/common-graphical.nix
+++ modules/common-graphical.nix
@@ -106,7 +106,6 @@ in
 
     # Enable networkmanager.
     networking.networkmanager.enable = lib.mkDefault true;
-    networking.networkmanager.wifi.backend = "iwd";
 
     services.avahi.enable = true;
 
