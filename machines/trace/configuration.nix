99d0d93cbf51f1dd235373cb41dd81d46c568b39
talyz <kim.lindberger@gmail.com>
2023-11-04 01:08:11 +0100

    trace: Clean up old roots in root reset script

--- machines/trace/configuration.nix
+++ machines/trace/configuration.nix
@@ -71,22 +71,39 @@
       options = [ "subvol=root" "noatime" ];
     };
 
-  boot.initrd.postDeviceCommands = lib.mkAfter ''
-    mkdir /btrfs_tmp
-    mount /dev/root_vg/root /btrfs_tmp
-    if [[ -e /btrfs_tmp/root ]]; then
-      mv /btrfs_tmp/root "/btrfs_tmp/old_root_$(date "+%Y-%m-%-d_%H:%M:%S")"
-    fi
-    btrfs subvolume create /btrfs_tmp/root
-    sync
-    umount /btrfs_tmp
-  '';
   boot.initrd =
+    let
+      rootReset = ''
+        mkdir /btrfs_tmp
+        mount /dev/root_vg/root /btrfs_tmp
+        if [[ -e /btrfs_tmp/root ]]; then
+            mkdir -p /btrfs_tmp/old_roots
+            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+            mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+        fi
+
+        delete_subvolume_recursively() {
+            IFS=$'\n'
+            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                delete_subvolume_recursively "/btrfs_tmp/$i"
+            done
+            btrfs subvolume delete "$1"
+        }
+
+        for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+            delete_subvolume_recursively "$i"
+        done
+
+        btrfs subvolume create /btrfs_tmp/root
+        umount /btrfs_tmp
+      '';
+    in
       {
         availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
         kernelModules = [ "dm-snapshot" ];
         luks.devices."cryptroot".device = "/dev/disk/by-uuid/e16e9123-b1bb-4480-8557-3bfcdd503a95";
         luks.devices."cryptroot".allowDiscards = true;
+        postDeviceCommands = lib.mkAfter rootReset;
       };
 
   fileSystems."/persistent" =
