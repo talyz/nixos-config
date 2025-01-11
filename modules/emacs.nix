94c197654b3ec42a5a300a10ae329a9d4bb31dd3
talyz <kim.lindberger@gmail.com>
2025-01-11 12:44:13 +0100

    emacs: Remove old package overrides

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -24,58 +24,19 @@ let
     };
     extraEmacsPackages = epkgs: [ epkgs.copilot epkgs.treesit-grammars.with-all-grammars ] ++ (cfg.extraPackages epkgs);
     override = epkgs: (cfg.extraOverrides epkgs) // {
-      # weechat = epkgs.melpaPackages.weechat;
-      weechat =  epkgs.melpaPackages.weechat.overrideAttrs (oldAttrs: oldAttrs // {
-        src = pkgs.fetchFromGitHub {
-          owner = "bqv";
-          repo = "weechat.el";
-          rev = "446868de424170be0584980d4dcc0859f7077d54";
-          sha256 = "12vbp35z3hgr9lqplc7ycf8n2rfd0zarr50arc9bqy5z14pf3biv";
-        };
-      });
-      nix-ts-mode = epkgs.melpaPackages.nix-ts-mode.overrideAttrs (oldAttrs: oldAttrs // {
-        src = pkgs.fetchFromGitHub {
-          owner = "antifuchs";
-          repo = "nix-ts-mode";
-          rev = "0ef4e663add03d026a1804f57ac7d5453a635b15";
-          sha256 = "sha256-jEUmhfLE7cFan4/PF4qBiEOLsjM3Q4iSDTlM+0CYwZg=";
-        };
-      });
       elpy = epkgs.melpaPackages.elpy;
       dracula-theme = epkgs.melpaPackages.dracula-theme.overrideAttrs (oldAttrs: oldAttrs // {
         src = dracula-emacs;
       });
-
-      # Install copilot.el
-      copilot = epkgs.trivialBuild {
-        pname = "copilot";
-        version = "2023-04-27";
-
-        packageRequires = with epkgs; [ dash editorconfig s ];
-
-        preInstall = ''
-          mkdir -p $out/share/emacs/site-lisp
-          cp -vr $src/dist $out/share/emacs/site-lisp
-        '';
-
-        src = pkgs.fetchFromGitHub {
-          owner = "zerolfx";
-          repo = "copilot.el";
-          rev = "7cb7beda89145ccb86a4324860584545ec016552";
-          sha256 = "sha256-57ACMikRzHSwRkFdEn9tx87NlJsWDYEfmg2n2JH8Ig0=";
-        };
-      };
     };
   });
 
   languageServers = with pkgs; [
     elixir_ls
     gopls
-    # ccls
     clang-tools
     cmake-language-server
     cmake
-    # rnix-lsp
     nil
     # nixd
     python3Packages.python-lsp-server
